62618c5d122a6f1edcae1053d40e729b057f60d7
Tiziana1502 <rossitiziana90@gmail.com>
2026-05-21 11:03:04 -0300

    21/05 finalizazr listado ordenado

---
+++ pryRossiArchivos/frmListadoOrdenado.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace pryRossiArchivos
+{
+    partial class frmListadoOrdenado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbListado = new System.Windows.Forms.GroupBox();
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            this.colCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLimite = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDeuda = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.cmbModo = new System.Windows.Forms.ComboBox();
+            this.lblCampo = new System.Windows.Forms.Label();
+            this.cmbCampo = new System.Windows.Forms.ComboBox();
+            this.lblModo = new System.Windows.Forms.Label();
+            this.gbListado.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbListado
+            // 
+            this.gbListado.Controls.Add(this.dgvListado);
+            this.gbListado.Controls.Add(this.btnListar);
+            this.gbListado.Controls.Add(this.cmbModo);
+            this.gbListado.Controls.Add(this.lblCampo);
+            this.gbListado.Controls.Add(this.cmbCampo);
+            this.gbListado.Controls.Add(this.lblModo);
+            this.gbListado.Location = new System.Drawing.Point(12, 21);
+            this.gbListado.Name = "gbListado";
+            this.gbListado.Size = new System.Drawing.Size(500, 343);
+            this.gbListado.TabIndex = 1;
+            this.gbListado.TabStop = false;
+            this.gbListado.Text = "Consulta Datos";
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.AllowUserToDeleteRows = false;
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCodigo,
+            this.colNombre,
+            this.colLimite,
+            this.colDeuda});
+            this.dgvListado.Location = new System.Drawing.Point(16, 77);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.RowHeadersVisible = false;
+            this.dgvListado.Size = new System.Drawing.Size(470, 182);
+            this.dgvListado.TabIndex = 4;
+            // 
+            // colCodigo
+            // 
+            this.colCodigo.HeaderText = "Código";
+            this.colCodigo.Name = "colCodigo";
+            // 
+            // colNombre
+            // 
+            this.colNombre.HeaderText = "Nombre";
+            this.colNombre.Name = "colNombre";
+            this.colNombre.Width = 170;
+            // 
+            // colLimite
+            // 
+            this.colLimite.HeaderText = "Limite Crédito";
+            this.colLimite.Name = "colLimite";
+            // 
+            // colDeuda
+            // 
+            this.colDeuda.HeaderText = "Deuda";
+            this.colDeuda.Name = "colDeuda";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Enabled = false;
+            this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListar.Location = new System.Drawing.Point(369, 293);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 27);
+            this.btnListar.TabIndex = 5;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // cmbModo
+            // 
+            this.cmbModo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModo.FormattingEnabled = true;
+            this.cmbModo.Location = new System.Drawing.Point(325, 32);
+            this.cmbModo.Name = "cmbModo";
+            this.cmbModo.Size = new System.Drawing.Size(119, 21);
+            this.cmbModo.TabIndex = 3;
+            this.cmbModo.SelectedIndexChanged += new System.EventHandler(this.cmbModo_SelectedIndexChanged);
+            // 
+            // lblCampo
+            // 
+            this.lblCampo.AutoSize = true;
+            this.lblCampo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCampo.Location = new System.Drawing.Point(37, 30);
+            this.lblCampo.Name = "lblCampo";
+            this.lblCampo.Size = new System.Drawing.Size(70, 20);
+            this.lblCampo.TabIndex = 0;
+            this.lblCampo.Text = "Campo:";
+            // 
+            // cmbCampo
+            // 
+            this.cmbCampo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCampo.FormattingEnabled = true;
+            this.cmbCampo.Location = new System.Drawing.Point(113, 32);
+            this.cmbCampo.Name = "cmbCampo";
+            this.cmbCampo.Size = new System.Drawing.Size(119, 21);
+            this.cmbCampo.TabIndex = 1;
+            this.cmbCampo.SelectedIndexChanged += new System.EventHandler(this.cmbCampo_SelectedIndexChanged);
+            // 
+            // lblModo
+            // 
+            this.lblModo.AutoSize = true;
+            this.lblModo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModo.Location = new System.Drawing.Point(261, 33);
+            this.lblModo.Name = "lblModo";
+            this.lblModo.Size = new System.Drawing.Size(58, 20);
+            this.lblModo.TabIndex = 2;
+            this.lblModo.Text = "Modo:";
+            // 
+            // frmListadoOrdenado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 387);
+            this.Controls.Add(this.gbListado);
+            this.Name = "frmListadoOrdenado";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Listado de Clientes Ordenados por...";
+            this.Load += new System.EventHandler(this.frmListadoOrdenado_Load);
+            this.gbListado.ResumeLayout(false);
+            this.gbListado.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbListado;
+        private System.Windows.Forms.DataGridView dgvListado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLimite;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDeuda;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.ComboBox cmbModo;
+        private System.Windows.Forms.Label lblCampo;
+        private System.Windows.Forms.ComboBox cmbCampo;
+        private System.Windows.Forms.Label lblModo;
+    }
+}
---
+++ pryRossiArchivos/frmListadoOrdenado.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryRossiArchivos
+{
+    public partial class frmListadoOrdenado : Form
+    {
+        public frmListadoOrdenado()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            cmbCampo.SelectedIndex = -1;
+            cmbModo.SelectedIndex = -1;
+
+            switch (cmbModo.SelectedIndex)
+            {
+                case 0:
+                    if (cmbCampo.SelectedIndex == 0)
+                    {
+                        clsArchivo.OrdenarPorCodigoAscendente();
+                    }
+                    else
+                    {
+                        clsArchivo.OrdenarPorCodigoDescendente();
+                    }
+                    break;
+                case 1:
+                    if (cmbCampo.SelectedIndex == 1)
+                    {
+                        clsArchivo.OrdenarPorNombreAscendente();
+                    }
+                    else
+                    {
+                        clsArchivo.OrdenarPorNombreDescendente();
+                    }
+                    break;
+                case 2:
+                    if (cmbCampo.SelectedIndex == 2)
+                    {
+                        clsArchivo.OrdenarPorLimiteAscendente();
+                    }
+                    else
+                    {
+                        clsArchivo.OrdenarPorLimiteDescendente();
+                    }
+                    break;
+                case 3:
+                    if (cmbCampo.SelectedIndex == 3)
+                    {
+                        clsArchivo.OrdenarPorDeudaAscendente();
+                    }
+                    else
+                    {
+                        clsArchivo.OrdenarPorDeudaDescendente();
+                    }
+                    break;
+            }
+
+            dgvListado.Rows.Clear();
+            for (Int32 i = 0; i < clsVector.IND; i++)
+            {
+                dgvListado.Rows.Add(clsVector.Clientes[i].Codigo, clsVector.Clientes[i].Nombre, clsVector.Clientes[i].Limite, clsVector.Clientes[i].Deuda);
+            }
+        }
+
+        private void frmListadoOrdenado_Load(object sender, EventArgs e)
+        {
+            cmbCampo.Items.Add("Código");
+            cmbCampo.Items.Add("Nombre");
+            cmbCampo.Items.Add("Limite Crédito");
+            cmbCampo.Items.Add("Deuda");
+
+            cmbModo.Items.Add("Ascendente");
+            cmbModo.Items.Add("Descendente");
+        }
+
+        private void cmbCampo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbCampo.SelectedIndex != -1)
+            {
+                btnListar.Enabled = true;
+            }
+            else
+            {
+                btnListar.Enabled = false;
+            }
+        }
+
+        private void cmbModo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbModo.SelectedIndex != -1)
+            {
+                btnListar.Enabled = true;
+            }
+            else
+            {
+                btnListar.Enabled = false;
+            }
+        }
+    }
+}
